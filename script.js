546c8d6bd45b179f9fde136000c6f65196384d4f
Alberto <betim009@gmail.com>
2024-02-24 21:22:38 -0300

    new style

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const divResultados = document.getElementById('resultados');
 
 const createElements = (string, array, data) => {
     divResultados.innerHTML += `
-    <div class="dias">
-        <h2>${string}</h2>
-        <h2>${data}</h2>
+    <div class="dias border border-3 rounded">
+        <div class="bg-dark text-white">
+            <h2>${string}</h2>
+            <h2>${data}</h2>
+        </div>
         ${array.map((e) =>
-        e.aluno === "Vago" ? `<p class="vago">${e.horario} ${e.aluno}</p>` :
-        `<p>${e.horario}  ${e.aluno}</p>`).join(" ")}
+        e.aluno === "Vago" ? `<p class="bg-dark text-white fw-bold p-1">${e.horario} ${e.aluno}</p>` :
+        `<p class="p-1 border-bottom border-dark">${e.horario}  ${e.aluno}</p>`).join(" ")}
     </div>
     `;
 }
