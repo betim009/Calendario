7ba6a7ded0565ceb78b97225bdaf94b25259663b
Alberto <betim009@gmail.com>
2025-11-03 18:26:41 -0300

    update

--- data.js
+++ data.js
@@ -47,7 +47,7 @@ export const data = {
             },
             {
                 horario: '17h',
-                aluno: 'Natalia'
+                aluno: 'Vago'
             },
             {
                 horario: '18h',
@@ -183,7 +183,7 @@ export const data = {
             },
             {
                 horario: '17h',
-                aluno: 'Natalia'
+                aluno: 'Vago'
             },
             {
                 horario: '18h',
@@ -235,7 +235,7 @@ export const data = {
             },
             {
                 horario: '13h',
-                aluno: 'Natalia'
+                aluno: 'Vago'
             },
             {
                 horario: '14h',
